131033fa9dee93d88068636711a299c324a990e8
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2020-02-06 22:35:54 +0200

    Add project files.

---
+++ MinionNames03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MinionNames03
+{
+    class Program
+    {
+        private static string connectionString = "Server=DESKTOP-4Q4QNM5\\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            int id = int.Parse(Console.ReadLine());
+
+            connection.Open();
+
+            using (connection)
+            {
+                string sqlText = @"SELECT Name FROM Villains WHERE Id = @Id
+
+                                   SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+
+                SqlCommand cmd = new SqlCommand(sqlText, connection);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Villian: {reader["Name"]}");
+                        Console.WriteLine($"{reader["m.Name"]} {reader["m.Age"]}");
+                    }
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ VilianNames02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace VilianNames02
+{
+    class Program
+    {
+        private static string connectionString = "Server=DESKTOP-4Q4QNM5\\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                string sqlText = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                    FROM Villains AS v 
+                                    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                    GROUP BY v.Id, v.Name 
+                                    HAVING COUNT(mv.VillainId) > 3 
+                                    ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand cmd = new SqlCommand(sqlText, connection);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
